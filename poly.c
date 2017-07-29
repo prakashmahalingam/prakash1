0310dfab3728a0d110833c747d48bd361b74ca23
m.prakash <prakashm731997@gmail.com>
2017-07-29 16:42:04 +0530

    Create poly.c

---
+++ poly.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int number, revNumber=0, rem=0,tempNumber;
+    printf("Enter an integer number: ");
+    scanf("%d", &number);
+    tempNumber=number;
+    while(tempNumber!=0)
+    {
+        rem=tempNumber%10;
+        revNumber=revNumber*10+rem;
+        tempNumber/=10;
+    }  
+    if(revNumber==number)  
+        printf("%d is a palindrome.", number);
+    else
+        printf("%d is not a palindrome.", number);
+}
